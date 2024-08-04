5a1aa4ebe656347ca02da245438d6050306a6e22
Adrian <122489074+adriankasper@users.noreply.github.com>
2024-08-04 17:00:01 +0300

    Update RustCombatLogger.py
    added autoupdater

--- RustCombatLogger.py
+++ RustCombatLogger.py
@@ -1,53 +1,67 @@
 import keyboard
 import time
 import os
+import requests
 
-# Set the console title
-os.system('title RustCombatLogger')
+def GetLatestVersion():
+    url = 'https://github.com/adriankasper/RustCombatLogger/releases/latest'
+    r = requests.get(url)
+    version = r.url.split('/')[-1]
+    return version
 
+def DownloadLatestVersion(latestVersion):
+    print("New version detected, downloading latest version: ", latestVersion)
+    return_code = os.system(RCL_UPDATER)
+    if return_code == 0:
+        print("Updating RCL")
+    else:
+        print(f"Updating failed: {return_code}.")
+    
 
-keywords = {"chest", "arm", "head", "leg", "hand", "generic", "stomach"}
-
-killStates = {"killed", "wounded", "projectile_los"}
-
-COOLDOWN_TIME = 0.5
-
-# Initialize global variables
-last_pressed_time = 0
-
-# Set up the path for rustpath.txt and create necessary directories
-rustPathFile = os.path.join(os.getenv('APPDATA'), 'RustCombatLogger', 'rustpath.txt')
-os.makedirs(os.path.dirname(rustPathFile), exist_ok=True)
-rustPath = ""
 def initialSetup():
     global rustPath
-    # Read or prompt for Rust executable path
-    if os.path.isfile(rustPathFile):
-        with open(rustPathFile, 'r') as file:
-            rustPath = file.readline().strip()
+    latestVersion = GetLatestVersion()
+    if os.path.isfile(saveDataFile):
+        with open(saveDataFile, 'r') as file:
+            lines = file.readlines()
+            rustPath = lines[0].strip()
+            if (CURRENT_VERSION != latestVersion):
+                DownloadLatestVersion(latestVersion)
+                time.sleep(2)
+                os._exit(0)
     else:
-        print('Enter this command into rust console: bind z "client.consoletoggle;combatlog;client.consoletoggle;"')
-        rustPathInput = input("Enter path where Rust.exe is located: ")
-        with open(rustPathFile, 'w') as file:
-            file.write(rustPathInput.strip())
-        rustPath = rustPathInput.strip()
+        print('Enter this command into Rust console: bind z "client.consoletoggle;combatlog;client.consoletoggle;"')
+        while True:
+            rustPathInput = input("Enter path where Rust.exe is located: ")
+            rustExePath = os.path.join(rustPathInput, "Rust.exe")
+            if os.path.isfile(rustExePath):
+                with open(saveDataFile, 'w') as file:
+                    file.write(rustPathInput.strip())
+                rustPath = rustPathInput.strip()
+                break
+            else:
+                os.system('cls')
+                print("The specified path does not contain Rust.exe. Please try again.")
+
+
+    os.system('mode con: cols=47 lines=55')  
     
-initialSetup()
-os.system('mode con: cols=47 lines=55')  
+
+
 
 def on_key_event(e):
-    global last_pressed_time
-    current_time = time.time()
+    global lastPressedTime
+    currentTime = time.time()
     
     if e.name == 'z':  
-        if current_time - last_pressed_time >= COOLDOWN_TIME:
-            last_pressed_time = current_time
+        if currentTime - lastPressedTime >= COOLDOWN_TIME:
+            lastPressedTime = currentTime
             os.system('cls')  # Clear the console
             
             print("Press 'Z' to refresh combatlog")
 
-            # Clear recent_lines at the start of each refresh
-            recent_lines = []
+            # Clear recentLines at the start of each refresh
+            recentLines = []
             logPath = os.path.join(rustPath, 'output_log.txt')
             
             try:
@@ -56,7 +70,7 @@ def on_key_event(e):
                         columns = line.split()
                         if len(columns) > 1 and columns[1] == "you" and columns[3] == "player" and 'assets/prefabs/weapons/' in line:
                             for i, element in enumerate(columns):
-                                if any(keyword in element for keyword in keywords):
+                                if any(keyword in element for keyword in logIdentifyingKeywords):
                                     if i + 3 < len(columns):
                                         try:
                                             damage = round(float(columns[i+2]) - float(columns[i+3]), 1)
@@ -66,12 +80,12 @@ def on_key_event(e):
                                                     killState = columns[i+4]
                                                 else:
                                                     killState = ""
-                                            recent_lines.append((health, damage, killState))
+                                            recentLines.append((health, damage, killState))
                                         except ValueError:
-                                            recent_lines.append(("Error", "Error", ""))
+                                            recentLines.append(("Error", "Error", ""))
                                     break
                         if (len(columns) > 1 and columns[1] == "attacker"):
-                            recent_lines = []
+                            recentLines = []
                             hiddenEventAmount = 0
                         if (len(columns) > 7):
                             if (columns[7] == "seconds"):
@@ -82,7 +96,7 @@ def on_key_event(e):
                 return
             
             # Get the last 10 entries and reverse them
-            lastRows = recent_lines[-50:]
+            lastRows = recentLines[-50:]
             reversedLastRows = list(reversed(lastRows))
             
             # Print the formatted table
@@ -99,9 +113,26 @@ def on_key_event(e):
             for health, damage, killState in reversedLastRows:
                 print(f"{health:<15} {damage:<15} {killState:<15}")
 
-# Set up the keyboard hook
+# Set the console title
+os.system('title RustCombatLogger')
+
+logIdentifyingKeywords = {"chest", "arm", "head", "leg", "hand", "generic", "stomach"}
+killStates = {"killed", "wounded", "projectile_los"}
+
+COOLDOWN_TIME = 0.5
+CURRENT_VERSION = "1.0"
+RCL_UPDATER = os.path.join(os.getenv('APPDATA'), 'RustCombatLogger', 'RCLUpdater.bat')
+lastPressedTime = 0
+
 keyboard.hook(on_key_event)
+
+# Set up the path for saveData.txt and create necessary directories
+saveDataFile = os.path.join(os.getenv('APPDATA'), 'RustCombatLogger', 'saveData.txt')
+os.makedirs(os.path.dirname(saveDataFile), exist_ok=True)
+
+initialSetup()
 
 print("Press 'Z' to show combatlog")
 while True:
     keyboard.wait('z') 
+
