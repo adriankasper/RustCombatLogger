52ba758265ebd6a3c5b754019f8eef3d9f321e09
Adrian <122489074+adriankasper@users.noreply.github.com>
2024-08-08 22:16:52 +0300

    Update CLBuilder.bat

--- CLBuilder.bat
+++ CLBuilder.bat
@@ -1,3 +1,5 @@
 @echo off
 pyinstaller --icon=CLIcon.ico RustCombatLogger.py
+copy "RCLUpdater.bat" "dist\RustCombatLogger"
+powershell -command "Compress-Archive -Path 'dist\RustCombatLogger' -DestinationPath 'dist\RustCombatLogger.zip'"
 pause
